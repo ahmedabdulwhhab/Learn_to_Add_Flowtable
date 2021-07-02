a3685fa318159618b2e3614439459110f98353ed
ahmedabdulwhhab <80491399+ahmedabdulwhhab@users.noreply.github.com>
2021-07-02 17:30:33 +0200

    Create flow_table_scenario2.sh

---
+++ flow_table_scenario2.sh
@@ -0,0 +1,9 @@
+## scenario 2
+curl -X POST -d '{"dpid": 1,"cookie": 1,"cookie_mask": 1,"table_id": 0,"idle_timeout": 3000,"hard_timeout": 3000,"priority": 2,"flags": 1,"match":{"in_port": 1,"dl_dst": "00:00:00:00:00:02","dl_src": "00:00:00:00:00:01"},"actions":[{"type":"OUTPUT","port": 3}]}' http://localhost:8081/stats/flowentry/add
+curl -X POST -d '{"dpid": 1,"cookie": 1,"cookie_mask": 1,"table_id": 0,"idle_timeout": 3000,"hard_timeout": 3000,"priority": 2,"flags": 1,"match":{"in_port": 3,"dl_dst": "00:00:00:00:00:01","dl_src": "00:00:00:00:00:02"},"actions":[{"type":"OUTPUT","port": 1}]}' http://localhost:8081/stats/flowentry/add
+
+curl -X POST -d '{"dpid": 3,"cookie": 1,"cookie_mask": 1,"table_id": 0,"idle_timeout": 3000,"hard_timeout": 3000,"priority": 2,"flags": 1,"match":{"in_port": 1,"dl_dst": "00:00:00:00:00:02","dl_src": "00:00:00:00:00:01"},"actions":[{"type":"OUTPUT","port": 2}]}' http://localhost:8081/stats/flowentry/add
+curl -X POST -d '{"dpid": 3,"cookie": 1,"cookie_mask": 1,"table_id": 0,"idle_timeout": 3000,"hard_timeout": 3000,"priority": 2,"flags": 1,"match":{"in_port": 2,"dl_dst": "00:00:00:00:00:01","dl_src": "00:00:00:00:00:02"},"actions":[{"type":"OUTPUT","port": 1}]}' http://localhost:8081/stats/flowentry/add
+
+curl -X POST -d '{"dpid": 2,"cookie": 1,"cookie_mask": 1,"table_id": 0,"idle_timeout": 3000,"hard_timeout": 3000,"priority": 2,"flags": 1,"match":{"in_port": 2,"dl_dst": "00:00:00:00:00:02","dl_src": "00:00:00:00:00:01"},"actions":[{"type":"OUTPUT","port": 3}]}' http://localhost:8081/stats/flowentry/add
+curl -X POST -d '{"dpid": 2,"cookie": 1,"cookie_mask": 1,"table_id": 0,"idle_timeout": 3000,"hard_timeout": 3000,"priority": 2,"flags": 1,"match":{"in_port": 3,"dl_dst": "00:00:00:00:00:01","dl_src": "00:00:00:00:00:02"},"actions":[{"type":"OUTPUT","port": 2}]}' http://localhost:8081/stats/flowentry/add
